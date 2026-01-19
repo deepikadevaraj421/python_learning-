714edbb436009a5cf415a7c7718a438abb158aca
Deepika <deepikadevaraj413@gmail.com>
2026-01-19 22:56:46 +0530

    second commit

---
+++ python day1(calculator).py
@@ -0,0 +1,17 @@
+num1=float(input("Enter first number: "))
+num2=float(input("Enter second number: "))
+opr=input("Enter operator (+, -, *, /): ")
+if opr=='+':
+    result=num1+num2
+    print("The result is:", result)
+elif opr=='-':
+    result=num1-num2
+    print("The result is:", result)
+elif opr=='*':
+    result=num1*num2
+    print("The result is:", result)
+elif opr=='/':
+    result=num1/num2
+    print("The result is:", result)
+else:
+    print("Invalid operator") 
